a2152a18dfe59e69a51f80d7cf18f300e4c67277
Jason Stredwick <jason.stredwick@gmail.com>
2024-09-10 16:17:31 -0700

    Missed update of copyright and removal of sample test.

--- tests/bidi/basic.test.cpp
+++ tests/bidi/basic.test.cpp
@@ -1,4 +1,4 @@
-// Copyright © 2024 Jason Stredwick
+// Copyright © 2014-2022 Muhammad Tayyab Akram
 
 #include <print>
 #include <string>
@@ -24,41 +24,3 @@ TEST(BidiBasicTests, test_Basic) {
 #endif
     EXPECT_TRUE(true);
 }
-
-
-#if 0
-    /* Extract the first bidirectional paragraph. */
-    SBAlgorithmRef bidiAlgorithm = SBAlgorithmCreate(&codepointSequence);
-    SBParagraphRef firstParagraph = SBAlgorithmCreateParagraph(bidiAlgorithm, 0, INT32_MAX, SBLevelDefaultLTR);
-    SBUInteger paragraphLength = SBParagraphGetLength(firstParagraph);
-
-    /* Create a line consisting of whole paragraph and get its runs. */
-    SBLineRef paragraphLine = SBParagraphCreateLine(firstParagraph, 0, paragraphLength);
-    SBUInteger runCount = SBLineGetRunCount(paragraphLine);
-    const SBRun *runArray = SBLineGetRunsPtr(paragraphLine);
-
-    /* Log the details of each run in the line. */
-    for (SBUInteger i = 0; i < runCount; i++) {
-        printf("Run Offset: %ld\n", (long)runArray[i].offset);
-        printf("Run Length: %ld\n", (long)runArray[i].length);
-        printf("Run Level: %ld\n\n", (long)runArray[i].level);
-    }
-
-    /* Create a mirror locator and load the line in it. */
-    SBMirrorLocatorRef mirrorLocator = SBMirrorLocatorCreate();
-    SBMirrorLocatorLoadLine(mirrorLocator, paragraphLine, (void *)bidiText);
-    const SBMirrorAgent *mirrorAgent = SBMirrorLocatorGetAgent(mirrorLocator);
-
-    /* Log the details of each mirror in the line. */
-    while (SBMirrorLocatorMoveNext(mirrorLocator)) {
-        printf("Mirror Index: %ld\n", (long)mirrorAgent->index);
-        printf("Actual Code Point: %ld\n", (long)mirrorAgent->codepoint);
-        printf("Mirrored Code Point: %ld\n\n", (long)mirrorAgent->mirror);
-    }
-
-    /* Release all objects. */
-    SBMirrorLocatorRelease(mirrorLocator);
-    SBLineRelease(paragraphLine);
-    SBParagraphRelease(firstParagraph);
-    SBAlgorithmRelease(bidiAlgorithm);
-#endif
